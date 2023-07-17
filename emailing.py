25ad0b21fe45fc9e355371287949fb578db8effe
Gordon Burns <gordonburns@live.co.uk>
2023-07-17 16:09:18 +0100

    added send email function and linked to main

--- emailing.py
+++ emailing.py
@@ -1,2 +1,26 @@
-def send_email():
-    print("test")
+import smtplib, ssl
+import creds
+import imghdr
+from email.message import EmailMessage
+
+username = creds.getusername()
+password = creds.get_password()
+receiver = creds.get_receiver()
+
+
+def send_email(image_path):
+    message = EmailMessage()
+    message["Subject"] = "Object Detected"
+    message.set_content("Hey! Something moved in the cameras scope")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(username,password)
+    gmail.sendmail(username, receiver,message.as_string())
+    gmail.quit
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import glob
 import cv2
+import numpy
 import time
 from datetime import datetime
 from emailing import send_email
@@ -15,47 +16,46 @@ while True:
     check, frame = video.read()
     now = datetime.now()
 
-
     grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     grey_frame_gau = cv2.GaussianBlur(grey_frame, (15, 15), 0)
-    #cv2.imshow("My Video", grey_frame_gau)
+    # cv2.imshow("My Video", grey_frame_gau)
 
     if first_frame is None:
         first_frame = grey_frame_gau
 
     delta_frame = cv2.absdiff(first_frame, grey_frame_gau)
-    #cv2.imshow("My Video", delta_frame)
+    # cv2.imshow("My Video", delta_frame)
 
-    thresh_frame = cv2.threshold(delta_frame, 60, 255,cv2.THRESH_BINARY)[1]
-    dil_frame = cv2.dilate(thresh_frame,None, iterations=2)
-   #cv2.imshow("My Video", dil_frame)
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    # cv2.imshow("My Video", dil_frame)
 
-    contours, check = cv2.findContours(dil_frame,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
         if cv2.contourArea(contour) < 10000:
             continue
-        x,y,w,h = cv2.boundingRect(contour)
-        rectangle = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255, 0), 3)
+        x, y, w, h = cv2.boundingRect(contour)
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
         if rectangle.any:
             status = 1
             cv2.imwrite(f"images/{count}.png", frame)
             count = count + 1
             all_images = glob.glob("images/*.png")
-            index = int(len(all_images)/ 2)
+            index = int(len(all_images) / 2)
             image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
     cv2.putText(img=frame, text=now.strftime("%A"), org=(30, 80),
                 fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=3, color=(255, 255, 255),
                 thickness=2, lineType=cv2.LINE_AA)
     cv2.putText(img=frame, text=now.strftime("%H:%M:%S"), org=(30, 140),
                 fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=3, color=(255, 0, 0),
                 thickness=2, lineType=cv2.LINE_AA)
-    cv2.imshow("Video",frame)
+    cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
 
     if key == ord("q"):
