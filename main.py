62a739c650f87d77a2c2aa3dfa9dd13231b95a46
Gordon Burns <gordonburns@live.co.uk>
2023-07-17 13:43:04 +0100

    Added foundation for sending email

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import cv2
 import time
+from emailing import send_email
 
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(1)
 time.sleep(0.5)
 
 first_frame = None
+status_list = []
 while True:
-
+    status = 0
     check, frame = video.read()
     grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     grey_frame_gau = cv2.GaussianBlur(grey_frame, (15, 15), 0)
-    cv2.imshow("My Video", grey_frame_gau)
+    #cv2.imshow("My Video", grey_frame_gau)
 
     if first_frame is None:
         first_frame = grey_frame_gau
 
     delta_frame = cv2.absdiff(first_frame, grey_frame_gau)
-    cv2.imshow("My Video", delta_frame)
+    #cv2.imshow("My Video", delta_frame)
 
     thresh_frame = cv2.threshold(delta_frame, 60, 255,cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame,None, iterations=2)
@@ -28,8 +30,14 @@ while True:
         if cv2.contourArea(contour) < 10000:
             continue
         x,y,w,h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255, 0), 3)
+        if rectangle.any:
+            status = 1
 
+    status_list.append(status)
+    status_list = status_list[-2:]
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
     cv2.imshow("Video",frame)
     key = cv2.waitKey(1)
 
